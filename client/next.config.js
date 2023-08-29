6a40724df423e331e7547054a89e4f6ec751be42
Nattapong Panthiya <nattapong130247@gmail.com>
2023-08-29 14:17:25 +0700

    import bug from Faelayis

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,6 @@
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		port:
-			process.env.NODE_ENV === "development"
-				? process.env["npm_package_scripts_PORT"] || process.env["PORT"]
-				: null,
-	},
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			config.plugins = [...config.plugins, new PrismaPlugin()];
-		}
-
-		return config;
-	},
+	reactStrictMode: true,
 };
 
 module.exports = nextConfig;
---
+++ client/src/pages/customcard.js
@@ -0,0 +1,13 @@
+import { Card } from "@nextui-org/react";
+import React from "react";
+
+export const CustomCard = () => (
+	<Card className="w-[200px] space-y-5 p-4 mt-5 ml-5" radius="2xl">
+		<div className="h-24 rounded-lg bg-default-300"></div>
+		<div className="space-y-3">
+			<div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+			<div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+			<div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+		</div>
+	</Card>
+);
