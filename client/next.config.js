7986afcafe8347234ade65426fff0fcd46b1e783
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-08-30 04:08:04 +0700

    fix: prisma not work on server

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,20 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+	env: {
+		port:
+			process.env.NODE_ENV === "development"
+				? process.env["npm_package_scripts_PORT"] || process.env["PORT"]
+				: null,
+	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
+		return config;
+	},
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig
