a9cbc2dfd2bd93056ff2549b1a6e92398150b3ad
faelayis <48393914+Faelayis@users.noreply.github.com>
2023-08-29 17:09:56 +0700

    fix(client): issues bug

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
 	root: true,
 	extends: ["next/core-web-vitals", "prettier", "../.eslintrc"],
 	plugins: ["prettier"],
-	ignorePatterns: ["node_modules", "dist"],
+	ignorePatterns: ["node_modules", "src/pages/api"],
 	parserOptions: {
 		babelOptions: {
 			presets: [require.resolve("next/babel")],
