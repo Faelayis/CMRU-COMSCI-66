73a3f48a615f95892ce38b1616b8e26fd2ea79ef
Faelayis <Faelayis@users.noreply.github.com>
2023-10-19 11:49:17 +0000

    chore: fix problems

--- client/src/pages/contents/setting.js
+++ client/src/pages/contents/setting.js
@@ -19,7 +19,7 @@ export default function Setting() {
 						{session?.user ? (
 							<>
 								<Avatar
-									className="text-large h-20 w-20"
+									className="h-20 w-20 text-large"
 									isBordered
 									radius="full"
 									src={session.user?.image}
